0b152ba5bd0b6c3aca1ca4cb7f1ff21a9f58c962
Mohammad Hosein Kahkeshan <mo.ho.kahkeshan@gmail.com>
2026-07-09 22:20:44 +0330

    Add Grpc product service

---
+++ gRPC_AspNetCore/Services/GrpcProductService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using gRPC_AspNetCore.Protos;
+
+namespace gRPC_AspNetCore.Services;
+
+public class GrpcProductService : ProductService.ProductServiceBase
+{
+    public override Task<CreateProductReply> CreateProduct(IAsyncStreamReader<CreateProductRequest> requestStream, ServerCallContext context)
+    {
+        return base.CreateProduct(requestStream, context);
+    }
+}
